ea2bc85fd39815058c3a178e0d26c8275f5dbb7a
Laurent Senta <laurent.senta@gmail.com>
2020-04-28 11:02:38 +0200

    api: submit xml

--- __tests__/e2e/api.ts
+++ __tests__/e2e/api.ts
@@ -1,6 +1,36 @@
+import FormData from 'form-data'
+import fs from 'fs'
+import { IncomingMessage } from 'http'
 import fetch from 'isomorphic-unfetch'
 
 const API_URL = 'http://localhost:3000/api'
+
+// const dbg = (f: any): any => {
+//     return (...args: any[]) => {
+//         console.log('<<< INPUT:', args)
+//         const result = f(...args)
+//         console.log('>>> RESULT:', result)
+//         return result
+//     }
+// }
+
+const loadZip = (path: string): FormData => {
+  const form = new FormData()
+  form.append('testFile', fs.createReadStream(path))
+  return form
+}
+
+const consumeJSON = (x: IncomingMessage): any => {
+  return new Promise((resolve) => {
+    let body = ''
+    x.on('data', (chunk) => {
+      body += chunk
+    })
+    x.on('end', () => {
+      resolve(JSON.parse(body))
+    })
+  })
+}
 
 describe('status api', () => {
   it('returns 200', async () => {
@@ -16,6 +46,20 @@ describe('POST Results API', () => {
       headers: {}
     })
     expect(r.status).toEqual(201)
+  })
+
+  it('can upload data', async () => {
+    const formData = loadZip('./__tests__/e2e/fixtures/test-reports-1.zip')
+
+    const r = await new Promise<IncomingMessage>((resolve, reject) => {
+      formData.submit(`${API_URL}/results/my-job-id`, (err, res) =>
+        err ? reject(err) : resolve(res)
+      )
+    })
+
+    expect(r.statusCode).toEqual(201)
+    const json = await consumeJSON(r)
+    expect(json['xmlFilesCount']).toEqual(16)
   })
 })
 
