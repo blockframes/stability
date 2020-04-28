836f389d447744159cb1e75f5a44b365c7a69459
hugo-hontans <hugohontans@gmail.com>
2020-04-28 17:29:43 +0200

    continue to implement parseXML

--- src/utils/result.test.ts
+++ src/utils/result.test.ts
@@ -14,6 +14,33 @@ const ENTRY_1 = {
 </testsuites>`
 }
 
+const ENTRY_2 = {
+  name: 'catalog-4b1ce86199c62c265447c5458d03aee4.xml',
+  content: `<?xml version="1.0" encoding="UTF-8"?>
+  <testsuites name="Mocha Tests" time="0" tests="1" failures="1">
+    <testsuite name="Root Suite" timestamp="2020-03-30T09:18:22" tests="0" file="src/integration/dashboard/movie-tunnel-end.spec.ts" failures="0" time="0">
+    </testsuite>
+    <testsuite name="User can navigate to the movie tunnel, complete required fields, and send the movie" timestamp="2020-03-30T09:18:22" tests="1" failures="1" time="0">
+      <testcase name="User can navigate to the movie tunnel, complete required fields, and send the movie Login into an existing account, navigate on movie tunnel, complete required fields, go on titles page, navigate to movie page" time="0" classname="Login into an existing account, navigate on movie tunnel, complete required fields, go on titles page, navigate to movie page">
+        <failure message="Timed out retrying: Expected to find element: &apos;catalog-title-view catalog-title-details&apos;, but never found it." type="CypressError"><![CDATA[CypressError: Timed out retrying: Expected to find element: 'catalog-title-view catalog-title-details', but never found it.
+      at cypressErr (http://localhost:4200/__cypress/runner/cypress_runner.js:138036:9)
+      at throwErr (http://localhost:4200/__cypress/runner/cypress_runner.js:137969:11)
+      at Object.throwErrByPath (http://localhost:4200/__cypress/runner/cypress_runner.js:138017:3)
+      at retry (http://localhost:4200/__cypress/runner/cypress_runner.js:132299:19)
+      at onFailFn (http://localhost:4200/__cypress/runner/cypress_runner.js:120514:16)
+      at tryCatcher (http://localhost:4200/__cypress/runner/cypress_runner.js:164895:23)
+      at Promise._settlePromiseFromHandler (http://localhost:4200/__cypress/runner/cypress_runner.js:162831:31)
+      at Promise._settlePromise (http://localhost:4200/__cypress/runner/cypress_runner.js:162888:18)
+      at Promise._settlePromise0 (http://localhost:4200/__cypress/runner/cypress_runner.js:162933:10)
+      at Promise._settlePromises (http://localhost:4200/__cypress/runner/cypress_runner.js:163008:18)
+      at Async../node_modules/bluebird/js/release/async.js.Async._drainQueue (http://localhost:4200/__cypress/runner/cypress_runner.js:159620:16)
+      at Async../node_modules/bluebird/js/release/async.js.Async._drainQueues (http://localhost:4200/__cypress/runner/cypress_runner.js:159630:10)
+      at Async.drainQueues (http://localhost:4200/__cypress/runner/cypress_runner.js:159504:14)]]></failure>
+      </testcase>
+    </testsuite>
+  </testsuites>`
+}
+
 describe('Parse an array of xml to ITestResultGroup', () => {
   it('Return an empty array', async () => {
     const r = await parseXML([])
@@ -29,29 +56,56 @@ describe('Parse an array of xml to ITestResultGroup', () => {
     expect(r.apps.catalog.testName).toEqual(
       'User can navigate to the movie tunnel page 5, complete the fields, and navigate to page 6'
     )
-    expect(r).toEqual([
-      {
-        jobId: '',
-        apps: {
-          catalog: {
-            testName:
-              'User can navigate to the movie tunnel page 5, complete the fields, and navigate to page 6',
-            date: '2020-03-30T09:17:06',
-            successed: 1,
-            failed: 0,
-            file: 'src/integration/dashboard/movie-tunnel-4.spec.ts',
-            steps: [
-              {
-                success: true,
-                name:
-                  'Login into an existing account, navigate on budget page, complete budget and quotas fields, go on movie tunnel page 6',
-                duration: 24.86
-              }
-            ],
-            duration: 24.86
-          }
+    expect(r).toEqual({
+      jobId: '',
+      apps: {
+        catalog: {
+          testName:
+            'User can navigate to the movie tunnel page 5, complete the fields, and navigate to page 6',
+          date: '2020-03-30T09:17:06',
+          succeeded: 1,
+          failed: 0,
+          file: 'src/integration/dashboard/movie-tunnel-4.spec.ts',
+          steps: [
+            {
+              success: true,
+              name:
+                'Login into an existing account, navigate on budget page, complete budget and quotas fields, go on movie tunnel page 6',
+              duration: 24.86
+            }
+          ],
+          duration: 24.86
         }
       }
-    ])
+    })
+  })
+
+  it('Return an ITestResultGroup with error', async () => {
+    const r = await parseXML([ENTRY_2])
+
+    expect(r).toEqual({
+      jobId: '',
+      apps: {
+        catalog: {
+          testName:
+            'User can navigate to the movie tunnel, complete required fields, and send the movie',
+          date: '2020-03-30T09:18:22',
+          succeeded: 0,
+          failed: 1,
+          file: 'src/integration/dashboard/movie-tunnel-end.spec.ts',
+          steps: [
+            {
+              success: false,
+              name:
+                'Login into an existing account, navigate on movie tunnel, complete required fields, go on titles page, navigate to movie page',
+              duration: 0,
+              error:
+                "Timed out retrying: Expected to find element: 'catalog-title-view catalog-title-details', but never found it."
+            }
+          ],
+          duration: 0
+        }
+      }
+    })
   })
 })
--- src/utils/result.ts
+++ src/utils/result.ts
@@ -1,4 +1,5 @@
 import xml2js from 'xml2js'
+import { get } from 'lodash'
 
 export interface ITestResultGroup {
   jobId: string
@@ -10,7 +11,7 @@ export interface ITestResultGroup {
 export interface ITestResult {
   testName: string
   date: string
-  successed: number
+  succeeded: number
   failed: number
   file: string
   steps: IStep[]
@@ -29,18 +30,100 @@ export interface IUploadEntry {
   content: string
 }
 
+const content = {
+  testsuites: {
+    attr: { name: 'Mocha Tests', time: '0', tests: '1', failures: '1' },
+    testsuite: [
+      {
+        attr: {
+          name: 'Root Suite',
+          timestamp: '2020-03-30T09:18:22',
+          tests: '0',
+          file: 'src/integration/dashboard/movie-tunnel-end.spec.ts',
+          failures: '0',
+          time: '0'
+        }
+      },
+      {
+        attr: {
+          name:
+            'User can navigate to the movie tunnel, complete required fields, and send the movie',
+          timestamp: '2020-03-30T09:18:22',
+          tests: '1',
+          failures: '1',
+          time: '0'
+        },
+        testcase: [
+          {
+            attr: {
+              name:
+                'User can navigate to the movie tunnel, complete required fields, and send the movie Login into an existing account, navigate on movie tunnel, complete required fields, go on titles page, navigate to movie page',
+              time: '0',
+              classname:
+                'Login into an existing account, navigate on movie tunnel, complete required fields, go on titles page, navigate to movie page'
+            },
+            failure: [
+              {
+                _:
+                  "CypressError: Timed out retrying: Expected to find element: 'catalog-title-view catalog-title-details', but never found it.\n      at cypressErr (http://localhost:4200/__cypress/runner/cypress_runner.js:138036:9)\n      at throwErr (http://localhost:4200/__cypress/runner/cypress_runner.js:137969:11)\n      at Object.throwErrByPath (http://localhost:4200/__cypress/runner/cypress_runner.js:138017:3)\n      at retry (http://localhost:4200/__cypress/runner/cypress_runner.js:132299:19)\n      at onFailFn (http://localhost:4200/__cypress/runner/cypress_runner.js:120514:16)\n      at tryCatcher (http://localhost:4200/__cypress/runner/cypress_runner.js:164895:23)\n      at Promise._settlePromiseFromHandler (http://localhost:4200/__cypress/runner/cypress_runner.js:162831:31)\n      at Promise._settlePromise (http://localhost:4200/__cypress/runner/cypress_runner.js:162888:18)\n      at Promise._settlePromise0 (http://localhost:4200/__cypress/runner/cypress_runner.js:162933:10)\n      at Promise._settlePromises (http://localhost:4200/__cypress/runner/cypress_runner.js:163008:18)\n      at Async../node_modules/bluebird/js/release/async.js.Async._drainQueue (http://localhost:4200/__cypress/runner/cypress_runner.js:159620:16)\n      at Async../node_modules/bluebird/js/release/async.js.Async._drainQueues (http://localhost:4200/__cypress/runner/cypress_runner.js:159630:10)\n      at Async.drainQueues (http://localhost:4200/__cypress/runner/cypress_runner.js:159504:14)",
+                attr: {
+                  message:
+                    "Timed out retrying: Expected to find element: 'catalog-title-view catalog-title-details', but never found it.",
+                  type: 'CypressError'
+                }
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+}
+
 export async function parseXML(
   entries: IUploadEntry[]
 ): Promise<ITestResultGroup> {
   const apps: { [appName: string]: ITestResult } = {}
   await Promise.all(
     entries.map(async (entrie) => {
-      const parser = new xml2js.Parser()
+      const parser = new xml2js.Parser({ attrkey: 'attr' })
       const content = await parser.parseStringPromise(entrie.content)
-      console.log(content)
-      //@ts-ignore
+
+      const suites = content.testsuites.testsuite
+      const rootSuite = suites.filter(
+        (x: any) => x.attr.name === 'Root Suite'
+      )[0]
+      const ourDescribes = suites.filter(
+        (x: any) => x.attr.name !== 'Root Suite'
+      )
+
+      const fileName = rootSuite.attr.file
+
+      const testsuite = ourDescribes[0]
+
+      const steps = testsuite.testcase.map(
+        (test: any): IStep => {
+          const error = get(test, 'failure[0].attr.message', undefined)
+
+          return {
+            success: !error,
+            name: test.attr.classname,
+            duration: parseFloat(test.attr.time),
+            ...(error ? { error } : {})
+          }
+        }
+      )
+
       apps[entrie.name.split('-')[0]] = {
-        testName: entrie.content
+        file: fileName,
+        testName: testsuite.attr.name,
+        date: testsuite.attr.timestamp,
+        succeeded:
+          parseFloat(testsuite.attr.tests) -
+          parseFloat(testsuite.attr.failures),
+        failed: parseFloat(testsuite.attr.failures),
+        duration: parseFloat(testsuite.attr.time),
+        steps
       }
     })
   )
