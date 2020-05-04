43f174a1f1439ac3f0dabdd2d52a91c343e81aa1
Laurent Senta <laurent.senta@gmail.com>
2020-05-04 14:00:20 +0200

    results: extract describe fct

--- src/utils/result.ts
+++ src/utils/result.ts
@@ -33,8 +33,34 @@ export interface IUploadEntry {
   content: string
 }
 
-const parseDescribe = () => {
-  return {}
+const parseDescribe = (fileName: string, testsuite: any): ITestResult => {
+  const testCases = testsuite.testcase || []
+
+  const steps = testCases.map(
+    (test: any): IStep => {
+      const error = get(test, 'failure[0].attr.message', undefined)
+
+      return {
+        success: !error,
+        name: test.attr.classname,
+        duration: parseFloat(test.attr.time),
+        ...(error ? { error } : {})
+      }
+    }
+  )
+
+  const testResult: ITestResult = {
+    file: fileName,
+    testName: testsuite.attr.name,
+    date: testsuite.attr.timestamp,
+    succeeded:
+      parseFloat(testsuite.attr.tests) - parseFloat(testsuite.attr.failures),
+    failed: parseFloat(testsuite.attr.failures),
+    duration: parseFloat(testsuite.attr.time),
+    steps
+  }
+
+  return testResult
 }
 
 export async function parseXML(
@@ -43,51 +69,27 @@ export async function parseXML(
   const results = await Promise.all(
     entries.map(async (entry) => {
       try {
+        // Parse the XML
         const parser = new xml2js.Parser({ attrkey: 'attr' })
         const content = await parser.parseStringPromise(entry.content)
 
         const suites = content.testsuites.testsuite
 
+        // Mocha generate a "ROOT SUITE" that contains metadata, keep it aside
         const rootSuite = suites.filter(
           (x: any) => x.attr.name === 'Root Suite'
         )[0]
 
+        const fileName = rootSuite.attr.file
+        const appName = entry.name.split('-')[0]
+
+        // The other suites are our test, let's extract their data.
         const ourDescribes = suites.filter(
           (x: any) => x.attr.name !== 'Root Suite'
         )
 
-        const fileName = rootSuite.attr.file
-
         const testsuite = ourDescribes[0]
-
-        const testCases = testsuite.testcase || []
-
-        const steps = testCases.map(
-          (test: any): IStep => {
-            const error = get(test, 'failure[0].attr.message', undefined)
-
-            return {
-              success: !error,
-              name: test.attr.classname,
-              duration: parseFloat(test.attr.time),
-              ...(error ? { error } : {})
-            }
-          }
-        )
-        const appName = entry.name.split('-')[0]
-
-        const testResult: ITestResult = {
-          file: fileName,
-          testName: testsuite.attr.name,
-          date: testsuite.attr.timestamp,
-          succeeded:
-            parseFloat(testsuite.attr.tests) -
-            parseFloat(testsuite.attr.failures),
-          failed: parseFloat(testsuite.attr.failures),
-          duration: parseFloat(testsuite.attr.time),
-          steps
-        }
-
+        const testResult = parseDescribe(fileName, testsuite)
         return { appName, testResult }
       } catch (error) {
         console.error('processing:', entry.name, 'failed with error:', error)
