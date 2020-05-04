78d0140e91021640963cba38306ad4738f1591f0
Laurent Senta <laurent.senta@gmail.com>
2020-05-04 10:56:05 +0200

    db: add mongodb put and get

--- src/utils/db.test.ts
+++ src/utils/db.test.ts
@@ -1,5 +1,6 @@
 require('dotenv').config()
 import { loadDB } from './db'
+import { ITestResultGroup } from './result'
 
 describe('check mongo setup', () => {
   it('it exists', async () => {
@@ -8,12 +9,28 @@ describe('check mongo setup', () => {
 
   it('can connect and list the content', async () => {
     // arrange
-    const { db, client } = await loadDB('test-1')
+    const { collection } = await loadDB('test-1')
 
     // act
-    const count = await db.collection('jobResults').count()
+    const count = await collection.count()
 
-    // expect
+    // assert
     expect(count).toEqual(0)
+  })
+
+  it('can add a test result and get it back', async () => {
+    // arrange
+    const { add, get, collection } = await loadDB('test-2')
+    const someJob = { jobId: 'helloworld' }
+
+    // act
+    await add((someJob as unknown) as ITestResultGroup)
+    const retrieved = await get(someJob.jobId)
+    const count = await collection.count()
+
+    // assert
+    expect(count).toEqual(1)
+    expect(retrieved).toBeTruthy()
+    expect(retrieved.jobId).toEqual(someJob.jobId)
   })
 })
--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,4 +1,5 @@
 import { MongoClient } from 'mongodb'
+import { ITestResultGroup } from './result'
 require('dotenv').config()
 
 const MONGO_URL = process.env.MONGO_URL
@@ -12,5 +13,24 @@ export const loadDB = async (dbNameSuffix: string = '') => {
   const client = await MongoClient.connect(MONGO_URL)
   console.log('Connected successfully to server')
   const db = client.db(MONGO_DB_NAME + dbNameSuffix)
-  return { db, client }
+  const collection = db.collection('jobResults')
+
+  const add = async (job: ITestResultGroup): Promise<void> => {
+    const doc = { ...job, _id: job.jobId }
+    await collection.updateOne(
+      { _id: job.jobId },
+      {
+        $set: { ...doc, updated: new Date() },
+        $setOnInsert: { created: new Date() }
+      },
+      { upsert: true }
+    )
+  }
+
+  const get = async (jobID: string): Promise<ITestResultGroup> => {
+    const doc = await collection.findOne({ _id: jobID })
+    return doc
+  }
+
+  return { db, client, add, get, collection }
 }
