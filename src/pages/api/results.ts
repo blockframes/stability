8afe86b6bd11857b3d934723393dd96c19b579c3
Laurent Senta <laurent.senta@gmail.com>
2020-05-26 15:53:23 +0200

    spike in the slack notif

--- src/pages/api/results.ts
+++ src/pages/api/results.ts
@@ -3,10 +3,14 @@ import { createReadStream } from 'fs'
 import { flatten } from 'lodash'
 import { NextApiRequest, NextApiResponse } from 'next'
 import unzipper from 'unzipper'
-import { parseXML } from '@utils/result'
+import { parseXML, ITestResultGroup } from '@utils/result'
 import { loadDB } from '@utils/db'
+import axios from 'axios'
 
 const SECRET = process.env.SECRET
+const SLACK_HOOK = process.env.SLACK_WEBHOOK
+const DASHBOARD_API_URL = process.env.DASHBOARD_API_URL
+
 // test cmd:
 // http -f POST localhost:3000/results/test-jobID file@./__tests__/e2e/fixtures/test-reports-1.zip
 
@@ -81,12 +85,62 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
         const xmlFilesCount = xmlFiles.length
 
+        await pingSlack({ buildID, results: parsedEntries })
+
         return res
           .status(201)
           .json({ success: true, xmlFilesCount, buildID, metadata })
       })
     }
   }
+}
+
+const pingSlack = async ({
+  buildID,
+  results
+}: {
+  buildID: string
+  results: ITestResultGroup
+}): Promise<void> => {
+  const sumAll = (xs: number[]) => xs.reduce((x, y) => x + y, 0)
+
+  const totalStats = Object.entries(results.apps).reduce(
+    (current, [name, result]) => {
+      return {
+        success: current.success + sumAll(result.map((x) => x.succeeded)),
+        failed: current.success + sumAll(result.map((x) => x.failed))
+      }
+    },
+    { success: 0, failed: 0 }
+  )
+
+  const detailStats = Object.entries(results.apps).reduce(
+    (current, [name, result]) => {
+      const success = sumAll(result.map((x) => x.succeeded))
+      const failed = sumAll(result.map((x) => x.failed))
+
+      if (failed === 0) {
+        return (
+          current +
+          `- 🟢 ${name.toUpperCase()}: success! _(${success} tests passed)_\n`
+        )
+      } else {
+        return current + `- 🔴 ${name.toUpperCase()}: *${failed} failed*\n`
+      }
+    },
+    ''
+  )
+
+  const status = totalStats.failed === 0 ? 'succeeded' : 'failed'
+
+  return axios.post(SLACK_HOOK!, {
+    text: `
+Build ${buildID} ${status}!
+
+${detailStats}
+
+Check the build details on the <${DASHBOARD_API_URL}/${buildID}|dashboard>`
+  })
 }
 
 export default handler
